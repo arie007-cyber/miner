11e618ee3e97e865e3f8bda81da2fc4e5ee17f9f
arie007-cyber <59784007+arie007-cyber@users.noreply.github.com>
2021-06-02 12:29:34 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,5 @@
-#veruscoin
+# veruscoin
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVCGVRYmm2nStmTackV6AYgyUTGPmVFXi7.hellminer -p hybrid --cpu 4
+./hellminer.exe -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVCGVRYmm2nStmTackV6AYgyUTGPmVFXi7.saliman -p x --cpu 4
